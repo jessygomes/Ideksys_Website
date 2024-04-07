b6630e4ec4bd96034d13ea23aa079d93a9384a63
Jessy <you@example.com>
2024-04-07 15:38:21 +0200

    Ajout de la Navbar Responsive

---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,172 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
+import styles from "./Navbar.module.css";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+const itemNavbar = [
+  {
+    text: "Accueil",
+    link: "/",
+  },
+  {
+    text: "Nos Expertises",
+    subItems: [
+      {
+        title: "Sûreté",
+        link: "/surete",
+      },
+      {
+        title: "Sécurité Incendie",
+        link: "/securite-incendie",
+      },
+      {
+        title: "Eclairage de sécurité",
+        link: "/eclairage-de-securite",
+      },
+    ],
+  },
+  {
+    text: "Actualités",
+    link: "/actualites",
+  },
+  {
+    text: "A Propos",
+    link: "/a-propos",
+  },
+  {
+    text: "Contactez-nous",
+    link: "/contactez-nous",
+  },
+];
+
+export default function Navbar() {
+  const [navActive, setNavActive] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  //! DROPDOWN MENU
+  const [isFirstItemClicked, setIsFirstItemClicked] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  // Quand on clique à l'extérieur du dropdown, on ferme le dropdown
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsFirstItemClicked(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const dropdownRef = useRef(null);
+
+  //! BURGER TO CROSS + AFFICHE LA NAVBAR MOBILE
+  const [isActive, setIsActive] = useState(false);
+  const handleClick = () => {
+    setIsActive(!isActive);
+    setNavActive(!navActive);
+  };
+
+  //! NAVBAR SCROLL
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 50;
+      if (isScrolled !== scrolled) {
+        setScrolled(!scrolled);
+      }
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrolled]);
+
+  const pathname = usePathname();
+
+  return (
+    <header className={`${styles.header} ${scrolled ? styles.scrolled : ""}`}>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <Image
+            className={styles.headerLogo}
+            src="/Logo/ideksys_white_inline.png"
+            width={300}
+            height={300}
+            alt="Fresque logo"
+            onClick={() => setNavActive(false)}
+          />
+        </Link>
+        <div
+          className={`${styles.nav__menuBar} ${
+            isActive ? styles.activeBurger : ""
+          }`}
+          onClick={handleClick}
+        >
+          <div className={`${styles.line} ${styles.l1}`}></div>
+          <div className={`${styles.line} ${styles.l2}`}></div>
+          <div className={`${styles.line} ${styles.l3}`}></div>
+        </div>
+
+        <div
+          className={`${navActive ? styles.active : ""} ${
+            styles.nav__menuList
+          }`}
+        >
+          {itemNavbar.map((item, index) => {
+            const active =
+              item.link === "/"
+                ? pathname === "/"
+                : pathname.startsWith(item.link);
+            return (
+              <div
+                key={index}
+                onClick={() => {
+                  if (index === 1) {
+                    setIsFirstItemClicked(!isFirstItemClicked);
+                  } else if (!item.subItems) {
+                    setNavActive(false);
+                  }
+                }}
+                onMouseEnter={() => setActiveIndex(index)}
+                onMouseLeave={() => setActiveIndex(null)}
+              >
+                {index === 1 ? (
+                  <div className={styles.nav__item}>{item.text}</div>
+                ) : (
+                  <Link
+                    href={item.link}
+                    className={`${styles.nav__item} ${
+                      active ? styles.activeLink : ""
+                    }`}
+                  >
+                    {item.text}
+                  </Link>
+                )}
+                {activeIndex === index &&
+                  isFirstItemClicked &&
+                  item.subItems &&
+                  index === 1 && (
+                    <div ref={dropdownRef} className={styles.dropdownMenu}>
+                      {item.subItems.map((subItem, subIndex) => (
+                        <Link
+                          key={subIndex}
+                          href={subItem.link}
+                          className={styles.dropdownItem}
+                        >
+                          {subItem.title}
+                        </Link>
+                      ))}
+                    </div>
+                  )}
+              </div>
+            );
+          })}
+        </div>
+      </nav>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full bg-noir-fond">
+      <div className="fixed top-0 z-30 w-full flex justify-center">
+        <Navbar />
+      </div>
+      <body className="h-full">{children}</body>
     </html>
   );
 }
